29de2c50a6fb52191729af779292e8b570fb0149
AMY <AMY@DESKTOP-HSJRQSM>
2020-10-27 07:03:13 -0500

    Actualización Walter adición de operación suma  27/10/2020

--- src/appnetbeansgithub/Appnetbeansgithub.java
+++ src/appnetbeansgithub/Appnetbeansgithub.java
@@ -4,6 +4,9 @@ public class Appnetbeansgithub {
 
     public static void main(String[] args) {
         // Programa para verificar guardado de codigo fuente en Netbeans
+        int a=10, b=2, suma;
+        suma = a+b;
+        System.out.println("La suma es " + suma);
     }
     
 }
